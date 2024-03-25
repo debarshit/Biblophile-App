10d47e12fff5db0989388463482516410189a500
debarshi <dune02deb@gmail.com>
2024-03-25 11:05:34 +0530

    Basic template setup done

---
+++ src/navigators/TabNavigator.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { MaterialIcons, AntDesign } from '@expo/vector-icons';
+import { BlurView } from 'expo-blur';
+import {COLORS} from '../theme/theme';
+import HomeScreen from '../screens/HomeScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+import CartScreen from '../screens/CartScreen';
+import OrderHistoryScreen from '../screens/OrderHistoryScreen';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: styles.tabBarStyle,
+        tabBarBackground: () => (
+          <BlurView
+            intensity={15}
+            style={styles.BlurViewStyles}
+          />
+        ),
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <MaterialIcons
+              name="home"
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}></Tab.Screen>
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <MaterialIcons
+              name="shopping-cart"
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}></Tab.Screen>
+      <Tab.Screen
+        name="Favorite"
+        component={FavoritesScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <AntDesign
+              name="heart"
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}></Tab.Screen>
+      <Tab.Screen
+        name="History"
+        component={OrderHistoryScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <AntDesign
+              name="bells"
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}></Tab.Screen>
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    height: 80,
+    position: 'absolute',
+    backgroundColor: COLORS.primaryBlackRGBA,
+    borderTopWidth: 0,
+    elevation: 0,
+    borderTopColor: 'transparent',
+  },
+  BlurViewStyles: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+});
+
+export default TabNavigator;
