644784e0adba1684c5ac38541203f681b330fe7b
debarshi <dune02deb@gmail.com>
2025-05-15 12:34:52 +0530

    settings navigation moved to profile screen

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -5,66 +5,44 @@ import {COLORS, FONTFAMILY, FONTSIZE, SPACING} from '../theme/theme';
 import { useStore } from '../store/store';
 import instance from '../services/axios';
 import requests from '../services/requests';
-import ProfilePic from './ProfilePic';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 interface HeaderBarProps {
   title?: string;
 }
 
 const HeaderBar: React.FC<HeaderBarProps> = ({title}) => {
-
   const navigation = useNavigation<any>();
-
   const [streak, setStreak] = useState(null);
-
+  
   const userDetails = useStore((state: any) => state.userDetails);
 
-  useEffect(() => {
-    async function fetchCurrentStreak() {
-      try {
-        const response = await instance.post(requests.fetchReadingStreak, {
-          userId: userDetails[0].userId,
-        });
-        const data = response.data;
-        if (data.message === 1) {
-          setStreak(data.currentStreak);
-        }
-      } catch (error) {
-        console.error('Error fetching streak:', error);
+  const fetchCurrentStreak = async () => {
+    try {
+      const response = await instance.post(requests.fetchReadingStreak, {
+        userId: userDetails[0].userId,
+      });
+      const data = response.data;
+      if (data.message === 1) {
+        setStreak(data.currentStreak);
       }
+    } catch (error) {
+      console.error('Error fetching streak:', error);
     }
-  
-    fetchCurrentStreak();
-  }, [streak]);
+  };
 
-    // This useFocusEffect will run each time the component comes into focus
-    useFocusEffect(
-      React.useCallback(() => {
-        async function fetchCurrentStreak() {
-          try {
-            const response = await instance.post(requests.fetchReadingStreak, {
-              userId: userDetails[0].userId,
-            });
-            const data = response.data;
-            if (data.message === 1) {
-              setStreak(data.currentStreak);
-            }
-          } catch (error) {
-            console.error('Error fetching streak:', error);
-          }
-        }
-      
-        fetchCurrentStreak();
-      }, [])
+  useEffect(() => {
+    fetchCurrentStreak();
+  }, []);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchCurrentStreak();
+    }, [])
   );
 
   return (
     <View style={styles.HeaderContainer}>
-      {/* <GradientBGIcon
-        name="menufold"
-        color={COLORS.primaryLightGreyHex}
-        size={FONTSIZE.size_16}
-      /> */}
       <Image
           source={{uri: "https://ik.imagekit.io/umjnzfgqh/biblophile/common_assets/logos/Biblophile%20logo%20-%20white.png"}}
           style={styles.Image}
@@ -77,7 +55,10 @@ const HeaderBar: React.FC<HeaderBarProps> = ({title}) => {
       >
         <Text style={styles.StreakText}>{streak !== null && `Active Streak: ${streak} days`}</Text>
       </TouchableOpacity>
-      <ProfilePic />
+      
+      <TouchableOpacity onPress={() => navigation.navigate('Notifications')}>
+        <Ionicons name="notifications" size={24} color={COLORS.primaryWhiteHex} />
+      </TouchableOpacity>
     </View>
   );
 };
